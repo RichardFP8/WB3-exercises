bfd2ee841e320aea2ce2acbf82ae4dc5066c6cb8
Richard Perez <richard.floresperez0@gmail.com>
2022-10-24 13:12:12 -0400

    First exercise done

---
+++ Functions/simple_functions.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function favoriteThings(){
+    console.log("Richard Perez\nThor\nChuchoPonce");
+}
+function whyImHere(){
+    console.log("I joined because I want to learn more about software development for FREE");
+}
+function favoritePlace(){
+    console.log("The park because lots of trees");
+}
+favoriteThings();
+favoritePlace();
+whyImHere();
