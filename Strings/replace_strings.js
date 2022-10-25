6cad1e854827795d9a7f5ef7d18018a468e2c173
Richard Perez <richard.floresperez0@gmail.com>
2022-10-25 15:29:39 -0400

    Made modifications and used some date functions

---
+++ Strings/replace_strings.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// let message = "Our corporate offices are located in Dallas, dallas, DALLAS";
+// let change = message.replace("Dallas", "Austin");
+
+// //I don't understand the instructions so I tried this way...
+// console.log(message + "\n" + change + "\n" 
+// + message.match(/dallas/gi));
+
+let d = new Date();
+// console.log(d.toUTCString());
+// console.log(d.toString());
+// console.log(d.toDateString());
+
+// console.log(d.toLocaleDateString());
+// console.log(d.toLocaleTimeString());
+// console.log(d.toLocaleString());
+
+let date = new Date(1903, 4, 23);
+let test = new Date("2021-06-10T12:03:34");
+console.log(date);
+console.log(test);
